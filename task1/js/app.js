130c14238331077cdccfb28e299f25094e1f762a
Labunskyi Volodymyr <labunskyi@rambler.ru>
2019-04-16 14:57:20 +0300

    Add files via upload

---
+++ task1/js/app.js
@@ -0,0 +1,39 @@
+function Calculator(a, b){
+	
+	this.a = a;
+	this.b = b;
+	
+	this.setValA = function(a){
+		this.a = a;
+	}
+	
+	this.setValB = function(b){
+		this.b = b;
+	}
+	
+	
+	this.add = function() {
+		return this.a + this.b;
+	}
+	
+	this.sub = function() {
+		return this.a - this.b;
+	}
+	
+	this.mul = function() {
+		return this.a * this.b;
+	}
+	
+	this.dev = function () {
+		return this.a / this.b;
+	}
+}
+
+var calc = new Calculator(2, 2);
+
+
+calc.setValA(4);
+console.log(calc.add());
+console.log(calc.sub());
+console.log(calc.mul());
+console.log(calc.dev());
